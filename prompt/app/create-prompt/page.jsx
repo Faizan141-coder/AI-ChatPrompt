8466b6ad5bbaa4e5b518c3cbc2a91cd454a5fddb
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-06-26 12:17:32 +0500

    Feed Page Completed

--- prompt/app/create-prompt/page.jsx
+++ prompt/app/create-prompt/page.jsx
@@ -7,25 +7,47 @@ import { useRouter } from 'next/router'
 import Form from '@components/Form'
 
 const CreatePrmopt = () => {
-  const [submitting, setSubmitting] = useState(false)
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: ''
-  })
+    const router = useRouter()
+    const { data: session } = useSession()
 
-  const createPrmopt = async (e) => {
-    
-  }
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    })
+
+    const createPrmopt = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt, 
+                    userId: session ?.user.id,
+                    tag: post.tag
+                })
+            })
+
+            if (response.ok) {
+                router.push('/')
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setSubmitting(false)
+        }
+    }
   
-  return (
-    <Form 
-        type="Create"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={createPrmopt}
-    />
-  )
+    return (
+        <Form 
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrmopt}
+        />
+    )
 }
 
 export default CreatePrmopt
