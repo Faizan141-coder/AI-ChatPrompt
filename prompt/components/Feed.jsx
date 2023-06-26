4d32a067dc6cf8b4cb298857ba8f2a5a119b7aa3
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-06-26 14:24:19 +0500

    commit

--- prompt/components/Feed.jsx
+++ prompt/components/Feed.jsx
@@ -29,7 +29,12 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch('/api/prompt')
       const data = await response.json()
+
+      setPosts(data)
     }
+
+    console.log(posts)
+    fetchPosts()
   }, [])
 
   return (
