24e4e897d2b040d13089a7278dc0687bac514b5c
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-06-26 10:54:16 +0500

    commit

--- prompt/app/create-prompt/page.jsx
+++ prompt/app/create-prompt/page.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import Form from '@components/Form'
 
 const CreatePrmopt = () => {
+  const [submitting, setSubmitting] = useState(false)
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  })
+
+  const createPrmopt = async (e) => {
+    
+  }
+  
   return (
-    <div>CreatePrmopt</div>
+    <Form 
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrmopt}
+    />
   )
 }
 
