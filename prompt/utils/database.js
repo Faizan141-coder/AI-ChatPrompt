b8b79cc1d4e5fbdefec0e068097848e1d7d9d4ba
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-06-30 22:54:11 +0500

    final commit

--- prompt/utils/database.js
+++ prompt/utils/database.js
@@ -1,25 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let isConnected = false
+let isConnected  // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
+  mongoose.set('strictQuery', true);
 
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
+  
 
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            dbName: 'share_prompt',
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        isConnected = true
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "faizan",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.log(error);
-    }
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 }
